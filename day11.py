1b52ba5e1c82162c3d7f658586405ec3acfd3418
gurungyo <gurungy@dickinson.edu>
2025-12-11 20:15:43 -0500

    Final commit part 2 day 11

--- day11.py
+++ day11.py
@@ -5,22 +5,35 @@ for line in open("day11_input.txt"):
     source, targets = line.strip().split(": ")
     paths[source] = targets.split(" ")
 
-dic1 = {}
-def count_paths(node):
-    if node == 'out':
-        return 1
-    
-    if node not in paths:
-        return 0
-    
-    if node in dic1:
-        return dic1[node]
-    
-    total = 0
-    for neighbor in paths[node]:
-        total += count_paths(neighbor)
-    
-    dic1[node] = total
-    return total
+def count_paths(start, end):
+    dic1 = {}
 
-print(count_paths("you"))
+    def search(node):
+        if node == end:
+            return 1
+        if node not in paths:
+            return 0
+        
+        if node in dic1:
+            return dic1[node]
+        
+        total = 0
+        for neighbor in paths[node]:
+            total += search(neighbor)
+        
+        dic1[node] = total        
+        return total
+
+    return search(start)
+
+paths_dac_first = (
+    count_paths('svr','dac') * count_paths('dac','fft') * count_paths('fft','out')
+)
+
+paths_fft_first = (
+    count_paths('svr','fft') * count_paths('fft','dac') * count_paths('dac','out')
+)
+
+total = paths_dac_first + paths_fft_first
+
+print(total)
